976d2f24200070a9269d44d80ba37e37fc4a9425
Guilherme Bueno Reis <guilhermebuenoreis.contact@gmail.com>
2025-11-18 22:28:20 -0300

    feat: create test e2e for create new account and authenticate and create questions and fetch recent questions

--- src/controller/authenticate.controller.e2e-spec.ts
+++ src/controller/authenticate.controller.e2e-spec.ts
@@ -1,10 +1,11 @@
 import type { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { hash } from 'bcryptjs';
 import { AppModule } from 'src/app.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 import request from 'supertest';
 
-describe('Create account (E2E)', () => {
+describe('Authenticate (E2E)', () => {
   let app: INestApplication;
   let prisma: PrismaService;
 
@@ -20,19 +21,22 @@ describe('Create account (E2E)', () => {
     await app.init();
   });
 
-  it(`/POST accounts`, async () => {
-    const response = await request(app.getHttpServer()).post('/accounts').send({
-      name: 'Jhon Doe',
+  it(`/POST sessions`, async () => {
+    await prisma.user.create({
+      data: {
+        name: 'Jhon Doe',
+        email: 'jhondoe@gmail.com',
+        password: await hash('123456', 8),
+      },
+    });
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
       email: 'jhondoe@gmail.com',
       password: '123456',
     });
     expect(response.statusCode).toBe(201);
-
-    const userOnDatabase = await prisma.user.findUnique({
-      where: {
-        email: 'jhondoe@gmail.com',
-      },
+    expect(response.body).toEqual({
+      access_token: expect.any(String),
     });
-    expect(userOnDatabase).toBeTruthy();
   });
 });
---
+++ src/controller/fetch-recent-question.controller.e2e-spec.ts
@@ -0,0 +1,74 @@
+import type { INestApplication } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Test } from '@nestjs/testing';
+import { AppModule } from 'src/app.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import request from 'supertest';
+
+describe('Fetch recent question (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  it(`/GET questions`, async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'Jhon Doe',
+        email: 'jhondoe@gmail.com',
+        password: '123456',
+      },
+    });
+
+    const accessToken = jwt.sign({ sub: user.id });
+
+    await prisma.question.createMany({
+      data: [
+        {
+          title: 'Question 01',
+          content: 'content',
+          slug: 'questions-01',
+          authorId: user.id,
+        },
+        {
+          title: 'Question 02',
+          content: 'content',
+          slug: 'questions-02',
+          authorId: user.id,
+        },
+        {
+          title: 'Question 03',
+          content: 'content',
+          slug: 'questions-03',
+          authorId: user.id,
+        },
+      ],
+    });
+
+    const response = await request(app.getHttpServer())
+      .get('/questions')
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    expect(response.statusCode).toBe(200);
+
+    expect(response.body).toEqual({
+      questions: [
+        expect.objectContaining({ title: 'Question 01' }),
+        expect.objectContaining({ title: 'Question 02' }),
+        expect.objectContaining({ title: 'Question 03' }),
+      ],
+    });
+  });
+});
